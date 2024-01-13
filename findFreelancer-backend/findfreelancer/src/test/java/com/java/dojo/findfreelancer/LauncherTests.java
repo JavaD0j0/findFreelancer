973ce7d9d7b2383992a7f5463f98e84c8f9ac3ac
Mario Rodriguez <marior6@uci.edu>
2024-01-12 18:11:06 -0600

    Added spring initializer code

---
+++ findFreelancer-backend/findfreelancer/src/test/java/com/java/dojo/findfreelancer/LauncherTests.java
@@ -0,0 +1,13 @@
+package com.java.dojo.findfreelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauncherTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
